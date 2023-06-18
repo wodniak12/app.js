8f08e6a396c232d0e64c28f217fe9865619cef0b
wodniak12 <72550912+wodniak12@users.noreply.github.com>
2023-06-18 11:02:21 +0200

    Initial commit

---
+++ components/Task.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+
+const Task = (props) => {
+    return (
+<View style={styles.item}>
+    <View style={styles.itemLeft}>
+        <View style={styles.square}>
+
+        </View>
+        <Text style={styles.itemText}>{props.text}
+        </Text>
+    </View>
+    <View style={styles.circular}>
+
+    </View>
+</View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+    item:{
+backgroundColor: '#fff',
+padding:15,
+borderRadius:10,
+flexDirection: 'row',
+alignItems: 'center',
+justifyContent: 'space-between',
+marginBottom: 20,
+},
+
+itemLeft:{
+
+flexDirection: 'row',
+alignItems: 'center',
+flexwrap: 'wrap',
+
+
+},
+square:{
+width:24,
+height:24,
+backgroundColor: '#55bcf6',
+opacity:0.4,
+borderRadius:5,
+marginRight:15,
+},
+
+itemText:{
+maxwidth: '80%',
+},
+
+circular:{
+width:12,
+height:12,
+backgroundColor: '#55bcf6',
+borderWidth: 2,
+borderRadius:5,
+}
+});
+export default Task;
